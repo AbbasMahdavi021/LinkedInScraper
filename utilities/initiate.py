9dc775866b5b2770701803179c26670bc234d23d
AbbasMahdavi021 <abbas.mahdavi021@gmail.com>
2024-05-14 03:34:16 -0700

    Improve Initiate function, UX/Error handeling

--- utilities/initiate.py
+++ utilities/initiate.py
@@ -1,12 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 
+from .linkedin_login import linkedin_login
+from .get_linkedin_credentials import get_linkedin_credentials
 
-from get_linkedin_credentials import get_linkedin_credentials
+path_to_driver = './drivers/chromedriver.exe'
 
 def initiate():
 
-    service = Service(executable_path=path)
+    service = Service(executable_path=path_to_driver)
     driver = webdriver.Chrome(service=service)
+    driver.minimize_window()
 
+    is_logged_in = False
+
+    while True:
+        linkedin_credentials = get_linkedin_credentials()
+        if linkedin_credentials:
+            is_logged_in = linkedin_login(driver, linkedin_credentials)
+            if is_logged_in:
+                print("Login successful. Proceeding with the scraping process.")
+                break
+            else:
+                print("Couldn't log in. Retrying...")
+        else:
+            print("Failed to get LinkedIn credentials. Skipping login process.")
+            break
+    
