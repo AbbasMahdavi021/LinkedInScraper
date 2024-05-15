d5babc1d2d3bb5b5812ba6b4ecf8d609cbeeccd8
Abbas Mahdavi <abbas.mahdavi021@gmail.com>
2024-05-15 03:07:41 -0700

    Handle failed logins, display credential errors, capcha verification handeling

--- utilities/auth/linkedin_login.py
+++ utilities/auth/linkedin_login.py
@@ -1,9 +1,8 @@
 import time
-
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementNotVisibleException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
 
 def linkedin_login(driver, linkedin_credentials):
     """
@@ -17,75 +16,110 @@ def linkedin_login(driver, linkedin_credentials):
     - bool: True if login is successful, False otherwise.
     """
     username, password = linkedin_credentials
-
-    error = ''
-
     try:
         # Open LinkedIn login page
         driver.get("https://www.linkedin.com/login")
 
         time.sleep(2)
 
-        #if already logged in and feedpage is accessed
+        # Check if already logged in and feed page is accessed
         curr_url = driver.current_url
         if curr_url == 'https://www.linkedin.com/feed/':
             return True
 
         # Wait for the username field to be visible
-        username_field = WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.ID, "username"))
-        )
-        password_field = driver.find_element(By.ID, "password")
-
-        # Clear fields (in case they are pre-filled)
-        username_field.clear()
-        password_field.clear()
-
-        # Enter credentials
-        username_field.send_keys(username)
-        password_field.send_keys(password)
-
-        # Find and click the sign-in button
-        sign_in_button = driver.find_element(By.XPATH, "//button[contains(@class, 'from__button--floating')]")
-        sign_in_button.click()
-
-        time.sleep(2)
-
-        # Check for errors related to username and password
         try:
+            username_field = WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located((By.ID, "username"))
+            )
+            password_field = WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located((By.ID, "password"))
+            )
 
-            # Check if the "challenge" element exists
-            time.sleep(3)
-            url = driver.current_url
-            if "checkpoint" in url:
-                error = "LinkedIn Challenge Verification Needed!"
-                driver.maximize_window()  # Maximize the window for human verification
-            error_username = driver.find_element(By.ID, "error-for-username")
-            if error_username.is_displayed():
-                error = error_username.text
+            # Clear fields (in case they are pre-filled)
+            username_field.clear()
+            password_field.clear()
 
-            error_password = driver.find_element(By.ID, "error-for-password")
-            if error_password.is_displayed():
-                error = error_password.text
+            # Enter credentials
+            username_field.send_keys(username)
+            password_field.send_keys(password)
+
+            # Find and click the sign-in button
+            sign_in_button = driver.find_element(By.XPATH, "//button[contains(@class, 'from__button--floating')]")
+            sign_in_button.click()
+
+            time.sleep(2)
+
+            try:
+                # Check if the "challenge" element exists
+                url = driver.current_url
+                if "checkpoint" in url:
+                    print("LinkedIn Challenge Verification Required!")
+                    print("Please Complete Verification, Re-Checking in 30 seconds!")
+                    time.sleep(2)
+                    driver.maximize_window()  # Maximize the window for human verification
+                    # Wait until redirected to the feed page
+            
+                WebDriverWait(driver, 10).until(EC.url_contains("/feed"))
+
+                driver.minimize_window()
+                print("Verification Complete!")
+
+            except TimeoutException:
+                driver.minimize_window()
+                res = credentials_error(driver)
+                if res == False:
+                    return False
+                else:
+                    return res
+            
+            res = credentials_error(driver)
+
+            if res == False:
+                "No Credential Errors!"
+            else:
+                return res
+
+            # Wait until redirected to the feed page (or any other desired page)
+            WebDriverWait(driver, 10).until(
+                EC.url_contains("/feed")
+            )
+
+            # Check if login was successful by waiting for the dashboard element
+            WebDriverWait(driver, 20).until(
+                EC.visibility_of_element_located((By.CLASS_NAME, "feed-identity-module"))
+            )
+
+            print("Logged Into LinkedIn via Credentials")
+            return True
+
+        except TimeoutException:
+            return False
 
         except NoSuchElementException:
-            pass  # No error message for username
+            return False
 
-        # Wait until redirected to the feed page (or any other desired page)
-        WebDriverWait(driver, 5).until(
-            EC.url_contains("/feed")
-        )
-
-        # Check if login was successful by waiting for the dashboard element
-        WebDriverWait(driver, 5).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, "feed-identity-module"))
-        )
-        
-        print("Logged Into LinkedIn via Credentials")
-        driver.minimize_window()
-        return True
+        except Exception:
+            return False
 
     except Exception:
-        print (error)
-        driver.minimize_window()
+        print("That didn't work!")
         return False
+    
+
+
+def credentials_error(driver):
+    try:
+        # Check for errors related to username and password
+        error_username = driver.find_element(By.ID, "error-for-username")
+        if error_username.is_displayed():
+            return error_username.text
+        error_password = driver.find_element(By.ID, "error-for-password")
+        if error_password.is_displayed():
+            return error_password.text
+    except NoSuchElementException:
+        print("No credential errors!")
+        return False
+    
+    return False
+    
--- utilities/initiate.py
+++ utilities/initiate.py
@@ -3,16 +3,35 @@ from .auth.get_linkedin_credentials import get_linkedin_credentials
 
 def initiate(driver):
 
+    linkedin_credentials = get_linkedin_credentials()
+
+    error_count = 0
 
     while True:
-        linkedin_credentials = get_linkedin_credentials()
+
+        if error_count > 3:
+            skip_login = input("There is an issue logging in right now! Skip login (Y/N)? ").strip().lower()
+            if skip_login == 'y':
+                print("Skipping login.")
+                return False
+            else:
+                error_count = 0
+                print("Continuing login attempt...")
+
         if linkedin_credentials:
             is_logged_in = linkedin_login(driver, linkedin_credentials)
-            if is_logged_in:
+
+            if isinstance(is_logged_in, str):
+                error_count += 1
+                print(is_logged_in)
+                linkedin_credentials = get_linkedin_credentials()
+            elif is_logged_in:
                 print("Login successful. Now we can retrive more data!")
                 return True
             else:
                 print("Couldn't log in. Retrying...")
+                error_count += 1
         else:
             print("Proceeding without LinkedIn credentials.")
             return False
+            
